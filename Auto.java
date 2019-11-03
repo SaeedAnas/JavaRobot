5148b6ab2f32e3ff4fd23b5940ffaa75d4f785a9
SaeedAnas <52471189+SaeedAnas@users.noreply.github.com>
2019-11-02 19:12:01 -0700

    Add files via upload

--- Auto.java
+++ Auto.java
@@ -36,13 +36,20 @@ public class Auto extends LinearOpMode {
             DRIVE_GEAR_REDUCTION = 0.5, // this is < 1.0 if geared UP
             WHEEL_DIAMETER_INCHES = 4.0,
             COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * Math.PI),
-            DRIVE_SPEED = 0.5,
+            DRIVE_SPEED = 0.4,
             TURN_SPEED = 0.5,
             ARM_SPEED = 0.3,
             LEFT_FOUNDATION_DOWN = 0.4,
             LEFT_FOUNDATION_UP = 0.9,
             RIGHT_FOUNDATION_DOWN = 0.6,
-            RIGHT_FOUNDATION_UP = 0.1;
+            RIGHT_FOUNDATION_UP = 0.1,
+            GEAR_IN = 32,
+            GEAR_OUT = 16,
+            GEAR_RATIO_ARM = GEAR_OUT/GEAR_IN,
+            P_TICKS = 28,
+            P_DISTANCE_PER_ROTATION = 20.8,
+            TICKS_PER_MM_ARM = (GEAR_RATIO_ARM * COUNTS_PER_MOTOR_REV)/P_DISTANCE_PER_ROTATION,
+            BLOCK_HEIGHT_MM = 101.6;
 
 
 
@@ -61,7 +68,11 @@ public class Auto extends LinearOpMode {
 
     // IMU sensor variables
     private BNO055IMU imu;
-    private Orientation OriginAngle = new Orientation();
+    private Orientation lastAngles = new Orientation();
+    double globalAngle, power = .30, correction;
+
+
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -99,6 +110,7 @@ public class Auto extends LinearOpMode {
         // run
         int count = 2;
         while (opModeIsActive()) {
+
              printStatus();
 //            // move to the foundation
 //            encoderDrive(DRIVE_SPEED, -DISTANCE_TO_FOUNDATION_START, 5.0);
@@ -127,41 +139,143 @@ public class Auto extends LinearOpMode {
 //                grabber.setPosition(0.3);
 //                grabber.setPosition(0.5);
 //                count++;
-            grabFoundation();
             releaseFoundation();
-            encoderDrive(DRIVE_SPEED, 1, 2.0);
             sleep(1000);
-            encoderDrive(DRIVE_SPEED,-1, 2.0);
+            encoderDrive(DRIVE_SPEED, -29, 2.0);
             sleep(1000);
-            encoderTurn(TURN_SPEED, 90, 5.0);
+            grabFoundation();
             sleep(1000);
-            encoderTurn(TURN_SPEED, 90, 5.0);
+            encoderDrive(1, 29, 5.0);
             sleep(1000);
+            releaseFoundation();
             encoderTurn(TURN_SPEED, -90, 5.0);
-            sleep(1000);
-            encoderTurn(TURN_SPEED, -90, 5.0);
-            sleep(1000);
-            moveArmMotor(-45, ARM_SPEED);
-            sleep(1000);
-            moveArmMotor(45,ARM_SPEED);
+            encoderDrive(DRIVE_SPEED, 12, 5.0);
+            encoderTurn(TURN_SPEED,-90, 5.0);
+            encoderDrive(DRIVE_SPEED, 12, 5.0);
+            encoderTurn(TURN_SPEED, -90,5.0);
 
 
 
+
+//
+//            encoderDrive(DRIVE_SPEED, 1, 2.0);
+//            sleep(1000);
+//            encoderDrive(DRIVE_SPEED,-1, 2.0);
+//            sleep(1000);
+//            encoderTurn(TURN_SPEED, 90, 5.0);
+//            sleep(1000);
+//            encoderTurn(TURN_SPEED, 90, 5.0);
+//            sleep(1000);
+//            encoderTurn(TURN_SPEED, -90, 5.0);
+//            sleep(1000);
+//            encoderTurn(TURN_SPEED, -90, 5.0);
+//            sleep(1000);
+//            moveArmMotor(-45, ARM_SPEED);
+//            sleep(1000);
+//            moveArmMotor(45,ARM_SPEED);
 
         }
         // stop
     }
 
+    private void getBlock() {
+
+    }
+
+//    private void moveArmVertical(double mm, double power){
+//        int ticksToMove =armMotor.getCurrentPosition() + (int)(mm * TICKS_PER_MM_ARM);
+//        armMotor.setTargetPosition(ticksToMove);
+//        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        armMotor.setPower(power);
+//        while() {
+//
+//        }
+//
+//
+//    }
+
+    private void resetAngle() {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+        globalAngle = 0;
+    }
+    private double getAngle() {
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+        if(deltaAngle < -180){
+            deltaAngle += 360;
+
+        }
+        else if (deltaAngle > 180) {
+            deltaAngle -= 360;
+        }
+        globalAngle += deltaAngle;
+        lastAngles = angles;
+        return globalAngle;
+    }
+
+    private double checkDirection() {
+        double correction, angle, gain = .10;
+
+        angle = getAngle();
+
+        if (angle == 0){
+            correction = 0;
+
+        }
+        else {
+            correction = -angle;
+        }
+        correction = correction * gain;
+        return correction;
+    }
+
+    private void rotate(int degrees, double power) {
+        double leftPower, rightPower;
+        resetAngle();
+
+        if (degrees < 0) {
+            leftPower = power;
+            rightPower = -power;
+
+        }
+        else if (degrees > 0) {
+            leftPower = -power;
+            rightPower = power;
+        }
+        else return;
+
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
+
+        if (degrees < 0) {
+            while (opModeIsActive() && getAngle() == 0) {}
+            while (opModeIsActive() && getAngle() > degrees) {}
+
+
+        }
+        else {
+            while (opModeIsActive() && getAngle() < degrees) {}
+
+            rightMotor.setPower(0);
+            leftMotor.setPower(0);
+            sleep(1000);
+            resetAngle();
+        }
+    }
+
+
+
     // Foundation code
     private void grabFoundation() {
         foundationLeft.setPosition(LEFT_FOUNDATION_DOWN);
         foundationRight.setPosition(RIGHT_FOUNDATION_DOWN);
-
     }
     private void releaseFoundation() {
         foundationLeft.setPosition(LEFT_FOUNDATION_UP);
         foundationRight.setPosition(RIGHT_FOUNDATION_UP);
     }
+
+
 
     // arm code
 
@@ -318,80 +432,83 @@ public class Auto extends LinearOpMode {
 
     }
 
+
     // Only use at the beginning of rotate - it resets the imu and gets the origin value
-    private void resetAngle()
-    {
-         initImu();
-         // get the origin
-         OriginAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
-
-
-    }
+//    private void resetAngle1()
+//    {
+//         initImu();
+//         // get the origin
+//         OriginAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+//
+//
+//    }
 
     // returns the current angle
-    private double getAngle()
+    private double getAngle1()
     {
         return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES).firstAngle;
     }
 
-    // turns robot using the imu
-    private void rotate(double degrees, double power)
-    {
-        double  leftPower, rightPower;
 
-        // restart imu movement tracking.
-        resetAngle();
-
-        // this is the target degree
-        degrees += OriginAngle.firstAngle;
-
-        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
-        // clockwise (right).
-
-        // turn right when degrees is less than 0
-        if (degrees < 0)
-        {   // turn right.
-            leftPower = power;
-            rightPower = -power;
-        }
-
-        // turn left when degrees is greater than 0
-        else if (degrees > 0)
-        {   // turn left.
-            leftPower = -power;
-            rightPower = power;
-        }
-        else return;
-
-        // set power to rotate.
-        leftMotor.setPower(leftPower);
-        rightMotor.setPower(rightPower);
-
-        // rotate until turn is completed.
-        // right turn
-        if (degrees < 0)
-        {
-            // On right turn we have to get off zero first.
-            while (opModeIsActive() && getAngle() > degrees) {
-                printStatus(degrees);
-            }
-        }
-        // left turn
-        else {
-
-            while (opModeIsActive() && getAngle() < degrees) {
-                printStatus(degrees);
-            }
-        }
-
-        // turn the motors off.
-        rightMotor.setPower(0);
-        leftMotor.setPower(0);
-
-        // wait for rotation to stop.
-        sleep(1000);
-
-    }
+//
+//    // turns robot using the imu
+//    private void rotate1(double degrees, double power)
+//    {
+//        double  leftPower, rightPower;
+//
+//        // restart imu movement tracking.
+//        resetAngle();
+//
+//        // this is the target degree
+//        degrees += OriginAngle.firstAngle;
+//
+//        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
+//        // clockwise (right).
+//
+//        // turn right when degrees is less than 0
+//        if (degrees < 0)
+//        {   // turn right.
+//            leftPower = power;
+//            rightPower = -power;
+//        }
+//
+//        // turn left when degrees is greater than 0
+//        else if (degrees > 0)
+//        {   // turn left.
+//            leftPower = -power;
+//            rightPower = power;
+//        }
+//        else return;
+//
+//        // set power to rotate.
+//        leftMotor.setPower(leftPower);
+//        rightMotor.setPower(rightPower);
+//
+//        // rotate until turn is completed.
+//        // right turn
+//        if (degrees < 0)
+//        {
+//            // On right turn we have to get off zero first.
+//            while (opModeIsActive() && getAngle() > degrees) {
+//                printStatus(degrees);
+//            }
+//        }
+//        // left turn
+//        else {
+//
+//            while (opModeIsActive() && getAngle() < degrees) {
+//                printStatus(degrees);
+//            }
+//        }
+//
+//        // turn the motors off.
+//        rightMotor.setPower(0);
+//        leftMotor.setPower(0);
+//
+//        // wait for rotation to stop.
+//        sleep(1000);
+//
+//    }
 
     // prints the imu status and all motor statuses
     private void printStatus() {
