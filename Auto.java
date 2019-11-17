6e1736bf82dbeb5945d62abd45a4cf5b1b5ee56a
SaeedAnas <52471189+SaeedAnas@users.noreply.github.com>
2019-11-16 22:14:49 -0800

    Add files via upload

--- Auto.java
+++ Auto.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -24,22 +25,21 @@ public class Auto extends LinearOpMode {
 
     // WARNING : CODE HAS NOT BEEN TESTED AND MAY NOT WORK
 
-    // TODO : Measure the Width of the drive train (inches) and input the value here
-
-    private static final double ROBOT_WIDTH = 18;
-
     // runtime
     private ElapsedTime runtime = new ElapsedTime();
     private ElapsedTime time = new ElapsedTime();
 
     // Encoder variables
     private static final double
+            ROBOT_WIDTH = 18,
+            ROBOT_LENGTH = 18,
+            TILE_LENGTH = 23.75,
             COUNTS_PER_MOTOR_REV = 1440,
             DRIVE_GEAR_REDUCTION = 0.5, // this is < 1.0 if geared UP
             WHEEL_DIAMETER_INCHES = 4.0,
             COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * Math.PI),
             DRIVE_SPEED = 0.4,
-            TURN_SPEED = 0.5,
+            TURN_SPEED = 0.3,
             ARM_SPEED = 0.3,
             LEFT_FOUNDATION_DOWN = 0.4,
             LEFT_FOUNDATION_UP = 0.9,
@@ -55,7 +55,8 @@ public class Auto extends LinearOpMode {
             TICKS_PER_MM_ARM = (GEAR_RATIO_ARM * COUNTS_PER_MOTOR_REV)/P_DISTANCE_PER_ROTATION,
             TICKS_PER_INCH_ARM = TICKS_PER_MM_ARM / 25.4,
             BLOCK_HEIGHT_MM = 101.6,
-            BLOCK_HEIGHT_INCHES = BLOCK_HEIGHT_MM / 25.4;
+            BLOCK_HEIGHT_INCHES = BLOCK_HEIGHT_MM / 25.4,
+            TIME_FOR_HORIZONTAL_ARM = 7;
 
 
 
@@ -74,16 +75,17 @@ public class Auto extends LinearOpMode {
 
     private CRServo armServo;
 
-
     // IMU sensor variables
     private BNO055IMU imu;
-    private Orientation lastAngles = new Orientation();
+    private Orientation OriginAngle = new Orientation();
+    private double deltaAngle;
     double globalAngle, power = .30, correction;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         //Init the hardware
+        initImu();
         leftMotor = hardwareMap.get(DcMotor.class, "left");
         rightMotor = hardwareMap.get(DcMotor.class, "right");
         armMotor = hardwareMap.get(DcMotor.class,"arm");
@@ -91,6 +93,7 @@ public class Auto extends LinearOpMode {
         foundationRight = hardwareMap.get(Servo.class, "rightFoundation");
         grabber = hardwareMap.get(Servo.class, "grabber");
         armServo = hardwareMap.get(CRServo.class, "armServo");
+
 
         // rightMotor is upside-down
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -106,50 +109,55 @@ public class Auto extends LinearOpMode {
         // initialize the imu
         //initImu();
 
-        final double DISTANCE_TO_FOUNDATION_START = 12;
-        final double FOUNDATION_SERVO_DEGREE = 0.5;
-        final double DISTANCE_TO_FIRST_BLOCK = 100;
-        final double DISTANCE_TO_FOUNDATION_BLOCK = 200;
-
         // wait for play
         waitForStart();
 
         // run
-        int count = 1;
         while (opModeIsActive()) {
-             printStatus();
-//             releaseFoundation();
-//             whileDrive(-DRIVE_SPEED,-30,-30,5.0);
-//             grabFoundation();
-//             whileDrive(1, 30,30,5.0);
-//             releaseFoundation();
-//             encoderTurnLeft(TURN_SPEED, 90,5.0);
-//            whileDrive(DRIVE_SPEED, 10,11,5.0);
-//            encoderTurnLeft(TURN_SPEED, 90,5.0);
-//            whileDrive(DRIVE_SPEED, 15,15,5.0);
-//            encoderTurnLeft(TURN_SPEED, 90,5.0);
-//            whileDrive(DRIVE_SPEED, 15,15,5.0);
-//            encoderTurnLeft(TURN_SPEED, 90,5.0);
-//            whileDrive(DRIVE_SPEED, 30, 30,5.0);
-            if(count == 1) {
-                encoderTurnLeft(TURN_SPEED, 90, 5.0);
-                encoderTurnRight(TURN_SPEED, 90, 5.0);
-                whileDrive(DRIVE_SPEED, 30, 30, 5.0);
-                moveArmMotor(10,0.2);
-                moveArmMotor(-10,0.2);
-            }
-            grabber.setPosition(GRABBER_GRAB);
-            sleep(1000);
-            grabber.setPosition(GRABBER_RELEASE);
-            sleep(1000);
-            count++;
-            moveArmMotor(10, 0.2);
-            }
+            gyroLeft(TURN_SPEED,90);
+            sleep(2000);
+            gyroRight(TURN_SPEED,90);
+            sleep(2000);
+            gyroLeft(TURN_SPEED,45);
+            sleep(2000);
+            gyroRight(TURN_SPEED,45);
+            sleep(2000);
 
         }
         // stop
     }
 
+    private void red() {
+        foundation(1);
+    }
+
+    private void blue() {
+        foundation(-1);
+    }
+
+    private void block() {
+    encoderTurnRight(TURN_SPEED, 90, 5.0);
+    whileDrive(DRIVE_SPEED, TILE_LENGTH * 3, 5.0);
+    encoderTurnLeft(TURN_SPEED, 90, 5.0);
+    whileDrive(DRIVE_SPEED, 90, 5.0);
+
+    }
+
+    private void foundation(int team) {
+        releaseFoundation();
+        whileDrive(-DRIVE_SPEED, -((TILE_LENGTH*2) - ROBOT_LENGTH), 5.0);
+        grabFoundation();
+        whileDrive(DRIVE_SPEED,50,5.0);
+        releaseFoundation();
+        gyroLeft(TURN_SPEED, 90 * team);
+        whileDrive(DRIVE_SPEED, TILE_LENGTH, 5.0);
+        gyroLeft(TURN_SPEED, 90 * team);
+        whileDrive(DRIVE_SPEED, 18.5 + ROBOT_LENGTH, 5.0);
+        gyroLeft(TURN_SPEED, 90 * team);
+        whileDrive(DRIVE_SPEED, 34.25/2, 5.0);
+        gyroLeft(TURN_SPEED, 90 * team);
+        whileDrive(DRIVE_SPEED, TILE_LENGTH, 5.0);
+    }
 
 
     private void moveArmServo(double power) {
@@ -195,40 +203,12 @@ public class Auto extends LinearOpMode {
 
     }
 
-    // Drive Using Encoder
-    /**
-     *
-     * @param power = motor power
-     * @param leftInches = how many inches you want leftMotor to go
-     * @param rightInches = how many inches you want rightMotor to go
-     * @param timeoutS = how long the drive should go on for before stopping (just so if we make a mistake it wont go on longer than the timeoutS)
-     */
-    public void encoderDrive(double power, double leftInches, double rightInches, double timeoutS) {
-        int newLeftTarget;
-        int newRightTarget;
-
-        if (opModeIsActive()) {
-            newLeftTarget = leftMotor.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            telemetry.addData("leftTarget", newLeftTarget);
-            newRightTarget = rightMotor.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-            telemetry.addData("rightTarget", newRightTarget);
-
-            leftMotor.setTargetPosition(-newLeftTarget);
-            rightMotor.setTargetPosition(-newRightTarget);
-
-            leftMotor.setPower(power);
-            rightMotor.setPower(power);
-
-            leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            leftMotor.setPower(0);
-            rightMotor.setPower(0);
-
-        }
-
+    public void whileDrive(double power, double distance, double timeoutS) {
+        whileDrive(power, distance,distance, timeoutS);
 
     }
+
+    // Drive Using Encoder
     public void whileDrive(double power, double leftInches, double rightInches, double timeoutS) {
         int newLeftTarget;
         int newRightTarget;
@@ -275,49 +255,9 @@ public class Auto extends LinearOpMode {
 
 
         }
-        
-    public void whileTurn(double power, double degree) {
-        if(opModeIsActive()) {
-            int newLeftTarget;
-            int newRightTarget;
-            if(degree < 0) {
 
-                newLeftTarget = leftMotor.getCurrentPosition() + (int)calcTurn(-degree);
-                newRightTarget = rightMotor.getCurrentPosition() + (int)calcTurn(degree);
-                leftMotor.setPower(-power);
-                rightMotor.setPower(power);
 
-                while (opModeIsActive() && leftMotor.getCurrentPosition() >= newLeftTarget && rightMotor.getCurrentPosition() <= newRightTarget) {
-                    telemetry.addData("WhileTurn", 10);
-                    telemetry.addData("CurrentLeft: ", leftMotor.getCurrentPosition());
-                    telemetry.addData("TargetRight: ", newLeftTarget);
-                    telemetry.addData("CurrentRight: ", rightMotor.getCurrentPosition());
-                    telemetry.addData("TargetRight: ", newRightTarget);
-                    telemetry.update();
-                }
-                leftMotor.setPower(0);
-                rightMotor.setPower(0);
-            }
-            else {
 
-                newLeftTarget = leftMotor.getCurrentPosition() + (int)calcTurn(degree);
-                newRightTarget = rightMotor.getCurrentPosition() + (int)calcTurn(-degree);
-                leftMotor.setPower(power);
-                rightMotor.setPower(-power);
-
-                while (opModeIsActive() && leftMotor.getCurrentPosition() <= newLeftTarget && rightMotor.getCurrentPosition() >= newRightTarget) {
-                    telemetry.addData("WhileTurn", 10);
-                    telemetry.addData("CurrentLeft: ", leftMotor.getCurrentPosition());
-                    telemetry.addData("TargetRight: ", newLeftTarget);
-                    telemetry.addData("CurrentRight: ", rightMotor.getCurrentPosition());
-                    telemetry.addData("TargetRight: ", newRightTarget);
-                    telemetry.update();
-                }
-                leftMotor.setPower(0);
-                rightMotor.setPower(0);
-            }
-        }
-    }
 
     public void resetEncoder() {
         leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -407,70 +347,120 @@ public class Auto extends LinearOpMode {
 
     }
 
-    private void encoderDrive(double power, double distance, double timeOutS) {
-        encoderDrive(power, -distance, -distance, timeOutS);
-    }
-
-    // turn using encoder
-
-    /**
-     *
-     * @param power = motor power
-     * @param degree = degree you want to turn by ( - for left, + for right)
-     * @param timeoutS = how long the drive should go on for before stopping (just so if we make a mistake it wont go on longer than the timeoutS)
-     */
-    public void encoderTurn(double power, double degree, double timeoutS) {
-        int newLeftTarget;
-        int newRightTarget;
-
-        if (opModeIsActive()) {
-            newLeftTarget = leftMotor.getCurrentPosition() + (int)calcTurn(-degree);
-            telemetry.addData("leftTarget", newLeftTarget);
-            newRightTarget = rightMotor.getCurrentPosition() + (int)calcTurn(degree);
-            telemetry.addData("rightTarget", newRightTarget);
-            leftMotor.setTargetPosition(newLeftTarget);
-            rightMotor.setTargetPosition(newRightTarget);
-
-            leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            runtime.reset();
-            leftMotor.setPower(Math.abs(power));
-            rightMotor.setPower(Math.abs(power));
-
-            while (opModeIsActive() && (runtime.seconds() < timeoutS) &&
-                    (leftMotor.isBusy() && rightMotor.isBusy())) {
-                telemetry.addData("RightMotor-Current", rightMotor.getCurrentPosition());
-                telemetry.addData("RightMotor-Target", newRightTarget);
-                telemetry.addData("LeftMotor-Current", leftMotor.getCurrentPosition());
-                telemetry.addData("LeftMotor-Target", newLeftTarget);
-                telemetry.update();
-
-            }
-
-            leftMotor.setPower(0);
-            rightMotor.setPower(0);
-            leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-
-
-    }
-
     private double calcTurn(double degree) {
         return (Math.PI * ROBOT_WIDTH * (degree / 360)) * COUNTS_PER_INCH;
     }
 
     // prints the imu status and all motor statuses
     private void printStatus() {
+        telemetry.addData("X", imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES).secondAngle);
+        telemetry.addData("Y", imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES).firstAngle);
+        telemetry.addData("Z",imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES).thirdAngle);
         telemetry.addData("LeftMotor", leftMotor.getCurrentPosition());
         telemetry.addData("RightMotor", rightMotor.getCurrentPosition());
         telemetry.addData("armMotor", armMotor.getCurrentPosition());
         telemetry.addData("grabber", grabber.getPosition());
         telemetry.addData("foundationLeft", foundationLeft.getPosition());
         telemetry.addData("foundationRight", foundationRight.getPosition());
+        telemetry.addData("ArmServo", armServo.getPower());
         telemetry.addData("Time", runtime.seconds());
         telemetry.update();
+    }
+
+    private void initImu() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
+    }
+
+    private double getAngle()
+    {
+        return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES).firstAngle;
+    }
+
+    private void rotate(double power, int degrees)
+    {
+
+        if(degrees == 0) {
+            return;
+        }
+
+        double targetDegree = getAngle() + degrees;
+
+        // if the target degree < 180, we add 360 and make it positive
+        // ex. getAngle() = -100, degrees = -90, -> targetDegree = -190, but the imu will not go to -190,
+        // but instead become 180. In this case, we would want the imu to stop when the degree is 170, so -190 + 360 = 170!
+        if (targetDegree < -180) {
+
+            targetDegree = (targetDegree + 360);
+            leftMotor.setPower(power);
+            rightMotor.setPower(-power);
+            // current = -100 -> 260, target = 170, degree is < 0, so it will turn right;
+            while(opModeIsActive() && (getAngle() + 360 > targetDegree)){
+                telemetry.addData("negative", 0);
+                telemetry.addData("targetDegree", targetDegree);
+                telemetry.addData("angle", getAngle());
+                printStatus();
+            }
+
+        }
+        // if the target degree > 180, we subtract 360 and make it negative
+        // ex. getAngle() = 100, degrees = 90, -> targetDegree = 190, but the imu will not go to 190,
+        // but instead become -180. In this case, we would want the imu to stop when the degree is -170, so 190 - 360 = -170!
+        else if (targetDegree > 180) {
+
+            targetDegree = (targetDegree - 360);
+            leftMotor.setPower(power);
+            rightMotor.setPower(-power);
+            // current = 100 -> -260, target = -170, degree > 0, so it will turn left;
+            while(opModeIsActive() && (getAngle() - 360 < targetDegree)){
+                telemetry.addData("negative", 0);
+                telemetry.addData("targetDegree", targetDegree);
+                telemetry.addData("angle", getAngle());
+                printStatus();
+            }
+
+        }
+
+        else if (degrees < 0) {
+            leftMotor.setPower(power);
+            rightMotor.setPower(-power);
+            while(opModeIsActive() && (getAngle() > targetDegree)){
+                telemetry.addData("negative", 0);
+                telemetry.addData("targetDegree", targetDegree);
+                telemetry.addData("angle", getAngle());
+                printStatus();
+            }
+
+        } else {
+            leftMotor.setPower(-power);
+            rightMotor.setPower(power);
+            while(opModeIsActive() && (getAngle() < targetDegree)){
+                telemetry.addData("+", 0);
+                telemetry.addData("targetDegree", targetDegree);
+                telemetry.addData("angle", getAngle());
+                printStatus();
+            }
+        }
+
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
+
+    }
+
+    private void gyroRight(double power, int degrees) {
+        rotate(power, -degrees);
+    }
+
+    private void gyroLeft(double power, int degrees) {
+        rotate(power, degrees );
     }
 
 
