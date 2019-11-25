51cbf8714f33097dcfad7976944af526601a2d30
SaeedAnas <52471189+SaeedAnas@users.noreply.github.com>
2019-11-24 23:53:39 -0800

    Add files via upload

--- programmingFolder/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/foundation/Foundation.java
+++ programmingFolder/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/foundation/Foundation.java
@@ -22,38 +22,38 @@ abstract class Foundation extends Autonomous {
         checkTeam(team);
         releaseFoundation();
         drive(-DRIVE_SPEED, -3);
-        encoderTurn(TURN_SPEED, -45 * turnVal, 5.0);
+        turnByGyro(TURN_SPEED, -45 * turnVal);
         drive(-DRIVE_SPEED, -30);
-        encoderTurn(TURN_SPEED, 45 * turnVal, 5.0);
+        turnByGyro(TURN_SPEED, 45 * turnVal);
         drive(-DRIVE_SPEED, -(TILE_LENGTH - 18));
         grabFoundation();
         sleep(1000);
         drive(DRIVE_SPEED, 5);
-        encoderTurnOneWheel(1, -90 * turnVal, 5.0);
+        turnByGyro(1, -90 * turnVal);
         releaseFoundation();
         drive(DRIVE_SPEED, TILE_LENGTH - 3);
         brake();
     }
 
-    private void moveFoundationVertical(char team) {
-        releaseFoundation();
-        drive(-DRIVE_SPEED, -(TILE_LENGTH + 12));
-        grabFoundation();
-        sleep(1000);
-        drive(DRIVE_SPEED, TILE_LENGTH + 7);
-        releaseFoundation();
-        encoderTurn(TURN_SPEED, -100 * turnVal, 5.0);
-        drive(DRIVE_SPEED, ROBOT_LENGTH);
-        encoderTurn(TURN_SPEED, -100 * turnVal, 5.0);
-        drive(DRIVE_SPEED, ROBOT_LENGTH + 15);
-        encoderTurn(TURN_SPEED, -95 * turnVal, 5.0);
-        drive(DRIVE_SPEED, TILE_LENGTH);
-        encoderTurn(TURN_SPEED, -95 * turnVal, 5.0);
-        drive(DRIVE_SPEED, TILE_LENGTH);
-        encoderTurn(TURN_SPEED, -95 * turnVal, 5.0);
-        drive(DRIVE_SPEED, TILE_LENGTH * 2);
-        brake();
-    }
+//    private void moveFoundationVertical(char team) {
+//        releaseFoundation();
+//        drive(-DRIVE_SPEED, -(TILE_LENGTH + 12));
+//        grabFoundation();
+//        sleep(1000);
+//        drive(DRIVE_SPEED, TILE_LENGTH + 7);
+//        releaseFoundation();
+//        turnByGyro(TURN_SPEED, -100 * turnVal);
+//        drive(DRIVE_SPEED, ROBOT_LENGTH);
+//        turnByGyro(TURN_SPEED, -100 * turnVal);
+//        drive(DRIVE_SPEED, ROBOT_LENGTH + 15);
+//        turnByGyro(TURN_SPEED, -95 * turnVal);
+//        drive(DRIVE_SPEED, TILE_LENGTH);
+//        turnByGyro(TURN_SPEED, -95 * turnVal);
+//        drive(DRIVE_SPEED, TILE_LENGTH);
+//        turnByGyro(TURN_SPEED, -95 * turnVal);
+//        drive(DRIVE_SPEED, TILE_LENGTH * 2);
+//        brake();
+//    }
 
     void currentFoundation(char team) {
         moveFoundationHorizontal(team);
